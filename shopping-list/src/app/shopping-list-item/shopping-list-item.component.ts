a3dba219df8e074ec14b21bb22dce90bf1523d0a
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-18 21:53:50 -0500

    feat: print one list using selector

--- shopping-list/src/app/shopping-list-item/shopping-list-item.component.ts
+++ shopping-list/src/app/shopping-list-item/shopping-list-item.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingItem } from './shopping-item';
 
 
 @Component({
   selector: 'shop-shopping-list-item',
-  templateUrl: './shopping-list-item.component.html',
-  styleUrls: ['./shopping-list-item.component.css']
+  templateUrl: './shopping-list-item.component.html'
 })
 export class ShoppingListItemComponent implements OnInit {
-	
+	@Input() item: ShoppingItem;
+	itemId: number;
+
   constructor() { }
 
   ngOnInit() {
