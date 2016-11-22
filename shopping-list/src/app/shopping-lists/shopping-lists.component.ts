d71f9c7fe267e9db52e405bfd090c68f73ee2ffc
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-22 18:30:28 -0500

    feat: added shopping-list service to serve data

--- shopping-list/src/app/shopping-lists/shopping-lists.component.ts
+++ shopping-list/src/app/shopping-lists/shopping-lists.component.ts
@@ -1,51 +1,22 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { ShoppingItem } from '../shopping-item';
 import { Item } from '../shared/item';
+import { ShoppingListService } from '../shared/shopping-list.service';
 
 @Component({
   selector: 'shop-shopping-lists',
   templateUrl: './shopping-lists.component.html'
 })
 export class ShoppingListsComponent implements OnInit {
-	shoppingListItems: Item[] = [
-    new Item('Coca-Cola', 1),
-    new Item('Water', 2)
-  ];
-
-  shoppingList: ShoppingItem[] = [
-  new ShoppingItem(
-    'Girls Night',
-    'Jewlery Making Party',
-    'http://dcobbestateliquidators.com/wp-content/uploads/2016/06/GB-Jewlery-Borches-necklace-001-150x150.jpg',
-    '11/21/2016',
-    this.shoppingListItems),
-  new ShoppingItem(
-    'New Years Bar Crowl',
-    'Jewlery Making Party',
-    'http://dcobbestateliquidators.com/wp-content/uploads/2016/06/GB-Jewlery-Borches-necklace-001-150x150.jpg',
-    '12/31/2016',
-    this.shoppingListItems),
-  new ShoppingItem(
-    'Jenny B-Day - Rollercoasters',
-    'Jewlery Making Party',
-    'http://dcobbestateliquidators.com/wp-content/uploads/2016/06/GB-Jewlery-Borches-necklace-001-150x150.jpg',
-    '07/28/2016',
-    []),
-  new ShoppingItem(
-    'Marks B-Day - Home Dinner',
-    'Jewlery Making Party',
-    'http://dcobbestateliquidators.com/wp-content/uploads/2016/06/GB-Jewlery-Borches-necklace-001-150x150.jpg',
-    '06/13/2016',
-    [])
-  ];
+  shoppingList: ShoppingItem[] = [];
 
 	@Output() listSelected = new EventEmitter<ShoppingItem>();
 	
 
-  constructor() { } 
+  constructor(private shoppingListService: ShoppingListService) { } 
 
   ngOnInit() {
-  	
+  	this.shoppingList = this.shoppingListService.getShoppingList();
   } 
 
   onSelected(item: ShoppingItem){    
