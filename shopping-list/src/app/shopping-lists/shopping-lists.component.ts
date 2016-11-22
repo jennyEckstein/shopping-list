48bb4d10f24dd72fcaf20640393bfd550b6d6f2c
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-22 17:40:56 -0500

    feat: added demo array list and display all items

--- shopping-list/src/app/shopping-lists/shopping-lists.component.ts
+++ shopping-list/src/app/shopping-lists/shopping-lists.component.ts
@@ -6,15 +6,17 @@ import { ShoppingItem } from '../shopping-item';
   templateUrl: './shopping-lists.component.html'
 })
 export class ShoppingListsComponent implements OnInit {
-	shoppingList: ShoppingItem[] = [];
+	shoppingList: ShoppingItem[] = [
+  new ShoppingItem('Girls Night','Jewlery Making Party','http://dcobbestateliquidators.com/wp-content/uploads/2016/06/GB-Jewlery-Borches-necklace-001-150x150.jpg','11/21/2016'),
+  new ShoppingItem('New Years Bar Crowl','Jewlery Making Party','http://dcobbestateliquidators.com/wp-content/uploads/2016/06/GB-Jewlery-Borches-necklace-001-150x150.jpg','12/31/2016'),
+  new ShoppingItem('Jenny B-Day - Rollercoasters','Jewlery Making Party','http://dcobbestateliquidators.com/wp-content/uploads/2016/06/GB-Jewlery-Borches-necklace-001-150x150.jpg','07/28/2016'),
+  new ShoppingItem('Marks B-Day - Home Dinner','Jewlery Making Party','http://dcobbestateliquidators.com/wp-content/uploads/2016/06/GB-Jewlery-Borches-necklace-001-150x150.jpg','06/13/2016')
+  ];
+  
 	@Output() listSelected = new EventEmitter<ShoppingItem>();
-	list = new ShoppingItem(
-		'Girls Night', 
-		'Jewlery Making Party', 
-		'http://dcobbestateliquidators.com/wp-content/uploads/2016/06/GB-Jewlery-Borches-necklace-001-150x150.jpg', 
-		'11/21/2016');
+	
 
-  constructor() { }
+  constructor() { } 
 
   ngOnInit() {
   	
