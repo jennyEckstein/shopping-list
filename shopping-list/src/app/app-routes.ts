8ed61255e483056810cd87192418253ae72cc70f
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-29 22:39:02 -0500

    feat: merged main routes with child routes
    the code would not work like in the tutorial, I found this solution on
    GitHub this is related to the new update

--- shopping-list/src/app/app-routes.ts
+++ shopping-list/src/app/app-routes.ts
@@ -1,11 +1,17 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListsComponent } from './shopping-lists/shopping-lists.component';
 import { ItemListComponent } from './item-list/item-list.component';
-import { APP_CHILD_ROUTES } from './child-routes';
+import { ShoppingListDetailStartComponent } from './shopping-list-item/shopping-list-detail-start.component';
+import { ShoppingItemDetailComponent } from './shopping-list-item/shopping-item-detail.component';
+import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 const APP_ROUTES_PROVIDERS: Routes = [
 	{path: '', redirectTo: '/shopping-list', pathMatch: 'full'},
-	{path: 'shopping-list', component: ShoppingListsComponent, children: APP_CHILD_ROUTES},
+	{path: 'shopping-list', component: ShoppingComponent},
+	{path: 'shopping-list/new', component: ShoppingListEditComponent},
+	{path: 'shopping-list/:id', component: ShoppingListDetailStartComponent},
+	{path: 'shopping-list/:id/edit', component: ShoppingListEditComponent},
 	{path: 'all_items', component: ItemListComponent}
 ];
 
